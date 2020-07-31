bf942801190dad30024cc102fb1da815811e2e9b
yazdanzn <>
2020-07-31 23:51:03 +0430

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on('message' , (message) => {
                 message.react('✅').then(() => message.react('❌'));
 
                 const filter = (reaction, user) => {
-	            return ['✅', '❌'].includes(reaction.emoji.name) && user.messageReaction.guild.member(roles.cache.has)   ;
+	            return ['✅', '❌'].includes(reaction.emoji.name) && user.messageReaction.guild.member.roles.cache.has("735063970951462923")   ;
                 };
                 
                 message.delete({timeout:60000})
